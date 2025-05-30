c72f95f2dac7f16d27a1e89ca7ea6536d614cf24
marcelo.oliveira <marcelo.oliveira@linx.com.br>
2025-05-30 11:48:05 -0300

    Project created

---
+++ backend/src/main/java/com/adenciesystems/ascatalog/AscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.adenciesystems.ascatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AscatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/adenciesystems/ascatalog/AscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adenciesystems.ascatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
